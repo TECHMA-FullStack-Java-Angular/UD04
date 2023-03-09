e52cf25f438c8df6b1fab3bcbcea9721184ee64a
Paula Sousa <110301198+PaulaSousa2014@users.noreply.github.com>
2023-03-09 16:42:36 +0100

    Comentarios

--- src/VariablesNumericas.java
+++ src/VariablesNumericas.java
@@ -1,3 +1,6 @@
+/*Ejercicio 1:
+ * Declara dos variables numéricas (con el valor que desees), muestra por consola la suma,
+ *  resta, multiplicación, división y módulo (resto de la división).*/
 
 public class VariablesNumericas {
 	public static void main(String args[]) {
